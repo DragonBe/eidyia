562a5843bf96161675f18ef02ef27b8adfc837b1
ddc <dennis@decockict.be>
2013-11-18 21:33:06 +0100

    upload zend cloud

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
 require '../vendor/autoload.php';
 
-use Eidyia\Service;
-
 $app = new Silex\Application();
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
-$app['autoloader']->registerNamespace('Eidyia','../src');
 
 $app->run();
