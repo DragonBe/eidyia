e5ca21d9070f9146bb344cc3a6f76e85b651470c
ddc <dennis@decockict.be>
2013-11-18 21:44:03 +0100

    upload zend cloud

--- public/index.php
+++ public/index.php
@@ -3,8 +3,4 @@ require '../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-});
-
 $app->run();
