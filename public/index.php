00b31480d68902014fc93ec7546a08077170825d
ddc <dennis@decockict.be>
2013-11-18 20:55:54 +0100

    update zend components

--- public/index.php
+++ public/index.php
@@ -1,11 +1,13 @@
 <?php
 require '../vendor/autoload.php';
 
+use Eidyia\Service;
+
 $app = new Silex\Application();
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
-$app['autoloader']->registerNamespace('Eidyia','..src');
+$app['autoloader']->registerNamespace('Eidyia','../src');
 
 $app->run();
